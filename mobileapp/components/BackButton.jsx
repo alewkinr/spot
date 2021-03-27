19033081033795320bdd410945dab960edb7bff5
Ramil Aleshkin <rraleshk@mts.ru>
2021-03-27 17:01:37 +0300

    🚨 Added linter and fixes

--- mobileapp/components/BackButton.jsx
+++ mobileapp/components/BackButton.jsx
@@ -1,14 +1,13 @@
-import {useNavigation} from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import {Button, View} from "react-native";
-import {ArrowBack} from "../constants/Symbols";
+import { Button } from "react-native";
+import { ArrowBack } from "../constants/Symbols";
 
-const BackButton = (props) => {
+const BackButton = () => {
     const navigation = useNavigation();
     return (<Button title={`${ArrowBack} Назад`}
-            onPress={() => navigation.goBack()}
-    />)
-}
+        onPress={() => navigation.goBack()}
+    />);
+};
 
-
-export {BackButton}
+export { BackButton };
