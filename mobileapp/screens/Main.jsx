772bc2bcd158890335fa9dff7e6b61bc93ea9808
Stas Dev <devstasyan@gmail.com>
2021-03-27 17:56:52 +0300

    FIX: Main Layout

--- mobileapp/screens/Main.jsx
+++ mobileapp/screens/Main.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Text, View, StyleSheet, Button, useWindowDimensions, Dimensions } from 'react-native';
+import { Text, SafeAreaView, View, StyleSheet, Button, Dimensions } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const Main = () => {
   const navigation = useNavigation();
 
-  return (<View style={styles.container}>
+  return (<SafeAreaView style={styles.container}>
     <Text style={styles.text}>Main</Text>
     <Button title="Press to Back" 
       onPress={() => navigation.goBack()}
     />
-  </View>);
+  </SafeAreaView>);
 }
 
 const styles = StyleSheet.create({
   container: {
-    minHeight: Math.round(Dimensions.get('window').height),
-    paddingTop: 40,
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: '#efefef',
+    flex: 1,
+    paddingTop: 40,
+    minHeight: Math.round(Dimensions.get('window').height),
   },
   text: {
     color: 'black',
