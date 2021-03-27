26645071900b541f23dccf497e378a8b2b5e9fd7
Haspel <>
2021-03-27 14:10:48 +0300

    Init commit

---
+++ back/src/test/java/ru/ad/astra/travel/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ad.astra.travel.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
