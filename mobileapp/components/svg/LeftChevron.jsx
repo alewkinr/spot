63fce5e7a642155fc0003676da255a6621de8bb7
Stas Dev <devstasyan@gmail.com>
2021-03-28 05:52:24 +0300

    ADD: LeftChevron icon

---
+++ mobileapp/components/svg/LeftChevron.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Svg, Path, } from "react-native-svg";
+
+// eslint-disable-next-line react/display-name
+export default (props) => {
+  return <Svg {...props} viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <Path d="M3.12587 5.69442L8.26854 0.551745C8.43879 0.38732 8.71009 0.392041 8.87452 0.562292C9.03492 0.728374 9.03492 0.99166 8.87452 1.15772L4.03482 5.99741L8.87452 10.8371C9.04183 11.0044 9.04183 11.2757 8.87452 11.4431C8.70715 11.6104 8.43588 11.6104 8.26854 11.4431L3.12587 6.30039C2.95855 6.13303 2.95855 5.86176 3.12587 5.69442Z" fill="#222A37" stroke="#222A37" strokeWidth="0.4" />
+  </Svg>
+}
